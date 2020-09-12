0976f405a5f22df0130dccf7074c9e01f2b837ba
MagdalenaFilipowicz <magdalenafilipowicznyc@gmail.com>
2020-09-13 01:59:25 +0200

    movies

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
 from flask import Flask, render_template
+from tmdb_client import get_popular_movies
+import tmdb_client
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    movies = []
-    return render_template("homepage.html", movies=movies)
+    movies = tmdb_client.get_movies(how_many=8)
     response = get_popular_movies()
-    return response
+    print(response)
+    return render_template("homepage.html", movies=movies)
+
+@app.context_processor
+def utility_processor():
+    def tmdb_image_url(path, size):
+        return tmdb_client.get_poster_url(path, size)
+    return {"tmdb_image_url": tmdb_image_url}
+
+@app.route("/movie/<movie_id>")
+def movie_details(movie_id):
+    details = tmdb_client.get_single_movie(movie_id)
+    return render_template("movie_details.html", movie=details)
 
 app.run()
